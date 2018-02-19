f6285616416901db54d131535c991cc65a7bfe9e
Tyler Fuqua <tyler@agilityda.com>
2018-02-18 19:00:03 -0700

    else if Statments

--- npm/index.js
+++ npm/index.js
@@ -1,15 +1,15 @@
-// Let
+// Conditional
 
-
-    let day = 26;
-    let month = "February";
-    let year = 1986;
-
-    let myBirthday = month + ' ' + day + ',' + ' ' + year;
-        
-
-
-    console.log(myBirthday);
+const age = '';
+    if( prompt( 'How old are you? ' + age ) < 10 ){
+        console.log("Not Permitted");
+    } else if( age < 15 ) {
+        console.log("Permitted with parent");
+    } else if( age < 18 ) {
+        console.log("Permitted with anyone over 18");
+    } else if( age > 18 ) {
+        console.log("Permitted to attend alone");
+    }
 
 
 
